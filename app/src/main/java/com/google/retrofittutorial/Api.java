f7a647a975a6432481bfee4c35ceba0b562d3ad6
Pierre <pierrevanderpol@gmail.com>
2018-09-30 13:46:38 +0200

    added OCR online parser

--- app/src/main/java/com/google/retrofittutorial/Api.java
+++ app/src/main/java/com/google/retrofittutorial/Api.java
@@ -1,8 +1,21 @@
 package com.google.retrofittutorial;
 
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 /**
  * Created by Pierre Vanderpol on 2018-09-27.
@@ -17,4 +30,50 @@ public interface Api {
     @GET("/Users")
     Call<ResponseBody> getUsers();
 
+    @POST("/users")
+    Call<ResponseBody> postUser(@Body RequestBody requestBody);
+
+    @GET("/Users")
+    Call<ResponseBody> getUsersByIds(@Query("id") List<Integer> ids);
+
+    @GET("/posts")
+    Call<ResponseBody> getPostsByUserId(@Query("userId") int userId);
+
+    @GET("/posts")
+    Call<ResponseBody> getPostsByUserIdAndPostId(@Query("userId") int userId, @Query("id") int postId);
+
+    @GET("/posts")
+    Call<ResponseBody> getPostsByIds(@Query("id") List<Integer> ids);
+
+    @GET("/posts")
+    Call<ResponseBody> getPostsByUserIdAndPostById(@Query("userId") String userId, @Query("id") String postId);
+
+    @GET("/posts")
+    Call<ResponseBody> getPostsByParams(@QueryMap Map<String, String>params);
+
+    // More dynamic with path
+    @GET("/posts/{id}")
+    Call<ResponseBody> getPostById(@Path("id") int id);
+
+    // Base URL
+    @GET("https://api.ipify.org")
+    Call<ResponseBody> getIp();
+
+    @GET
+    Call<ResponseBody> sendRequest(@Url String url);
+
+    @Multipart
+    @POST
+    Call<ResponseBody> uploadFile(@Url String url, @Part MultipartBody.Part part);
+
+    @Multipart
+    @POST
+    Call<ResponseBody> ocr(@Url String url,
+                          @Part("apikey") RequestBody apiKey,
+                          @Part("language") RequestBody language,
+                          @Part MultipartBody.Part part);
+
+
+
 }
+
--- app/src/main/java/com/google/retrofittutorial/MainActivity.java
+++ app/src/main/java/com/google/retrofittutorial/MainActivity.java
@@ -2,14 +2,7 @@ package com.google.retrofittutorial;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import java.io.IOException;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Api api = retrofit.create(Api.class);
+        /*
         api.getUsers().enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
@@ -41,7 +35,286 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
 
+        });*/
+
+
+
+        /*
+        String payload = "{\n" +
+                "\t\"id\": 100,\n" +
+                "\t\"name\": \"Gayrat Rakhimov\"\n" +
+                "}";
+
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), payload);
+
+        api.postUser(requestBody).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("Retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });*/
+
+        /*
+        api.getPostsByUserId(1).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
         });
+        */
+
+        /*
+        int userId = 1;
+        int postId = 2;
+
+        api.getPostsByUserIdAndPostId(userId, postId).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+        */
+
+            /*
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        api.getPostsByIds(list).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });*/
+
+            /*
+            String userId = "1";
+            String postId = null;
+            api.getPostsByUserIdAndPostById(userId, postId).enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    try {
+                        Log.d("retrofit", response.body().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                }
+            });*/
+
+        // Querymap annotation
+
+        /*Map<String, String> params = new HashMap<>();
+        params.put("userId","1");
+        params.put("id","2");
+
+        api.getPostsByParams(params).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+        */
+
+        // dynamic with path with annotation
+        /*
+        api.getPostById(1).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+        */
+
+        /*
+        api.getIp().enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+        */
+
+
+        /*
+        api.sendRequest("https://api.ipify.org").enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d("retrofit", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+        */
+
+        // SEND FILE
+        /*
+        try{
+            File file = new File(getCacheDir(), "hello.txt");
+            FileWriter writer = new FileWriter(file);
+            writer.append("hello");
+            writer.flush();
+            writer.close();
+
+            RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+            MultipartBody.Part part = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
+
+            api.uploadFile("https://file.io", part).enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    try {
+                        Log.d("retrofit", response.body().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+        /* OCR PARSER
+
+        final String OCR_URL = "https://api.ocr.space/parse/image";
+        final String API_KEY = "34eab8418788957";
+        final String LANGUAGE = "eng";
+
+        try {
+            InputStream input = getAssets().open("image.png");
+            File file = new File(getCacheDir(), "image.png");
+
+            try {
+                OutputStream output = new FileOutputStream(file);
+                try {
+                    try {
+                        byte[] buffer = new byte[1024];
+                        int read;
+
+                        while ((read = input.read(buffer)) != -1) {
+                            output.write(buffer, 0, read);
+                        }
+                        output.flush();
+                    } finally {
+                        output.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } finally {
+                input.close();
+            }
+            RequestBody apiKey = RequestBody.create(MediaType.parse("multipart/form-data"), API_KEY);
+            RequestBody language = RequestBody.create(MediaType.parse("multipart/form-data"), LANGUAGE);
+            final RequestBody requestBodyWithFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+
+            MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", file.getName(), requestBodyWithFile);
+            api.ocr(OCR_URL, apiKey, language, filePart).enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    try {
+                        Log.d("RetrofitExample", response.body().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                }
+            });
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    */
     }
+
 
 }
